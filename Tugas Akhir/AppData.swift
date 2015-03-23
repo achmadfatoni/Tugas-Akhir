be357b2e88dcb01ccdd0ef683850653d406f3feb
Achmad Fatoni <achmadfatony@gmail.com>
2015-03-23 14:47:26 +0700

    segue kota asal

---
+++ Tugas Akhir/AppData.swift
@@ -0,0 +1,13 @@
+//
+//  AppData.swift
+//  Tugas Akhir
+//
+//  Created by Achmad Fatoni on 3/23/15.
+//  Copyright (c) 2015 Achmad Fatoni. All rights reserved.
+//
+
+struct appData {
+    static var secretKey = "64de419c65901078dc7d026194357579"
+    static var token = ""
+    static var urlPath = "https://api.sandbox.tiket.com/"
+}
